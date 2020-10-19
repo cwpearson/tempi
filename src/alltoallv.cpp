730b857853233918bb26da0e076439269b24d135
Carl Pearson <carl.w.pearson@gmail.com>
2020-10-19 16:51:43 -0600

    set MPI_Alltoallv to staged_isir

--- src/alltoallv.cpp
+++ src/alltoallv.cpp
@@ -1,9 +1,9 @@
 #include "cuda_runtime.hpp"
 #include "env.hpp"
 #include "logging.hpp"
+#include "symbols.hpp"
 #include "topology.hpp"
 #include "types.hpp"
-#include "symbols.hpp"
 
 #include "allocator_slab.hpp"
 
@@ -93,7 +93,7 @@ int alltoallv_staged(PARAMS) {
   CUDA_RUNTIME(
       cudaMemcpy(hSendBuf, sendbuf, sendBufSize, cudaMemcpyDeviceToHost));
   err = libmpi.MPI_Alltoallv(hSendBuf, sendcounts, sdispls, sendtype, hRecvBuf,
-                      recvcounts, rdispls, recvtype, comm);
+                             recvcounts, rdispls, recvtype, comm);
   CUDA_RUNTIME(
       cudaMemcpy(recvbuf, hRecvBuf, recvBufSize, cudaMemcpyHostToDevice));
 
@@ -101,6 +101,63 @@ int alltoallv_staged(PARAMS) {
   CUDA_RUNTIME(cudaFreeHost(hSendBuf));
   CUDA_RUNTIME(cudaFreeHost(hRecvBuf));
   nvtxRangePop();
+  return err;
+}
+
+/* copy to host, and do a bunch of isend/irecv
+ */
+int alltoallv_staged_isir(PARAMS) {
+
+  int err = MPI_SUCCESS;
+
+  int commSize = 0;
+  MPI_Comm_size(comm, &commSize);
+
+  nvtxRangePush("alloc");
+  size_t sendBufSize = sdispls[commSize - 1] + sendcounts[commSize - 1];
+  size_t recvBufSize = rdispls[commSize - 1] + recvcounts[commSize - 1];
+  char *hSendBuf = {};
+  char *hRecvBuf = {};
+  CUDA_RUNTIME(cudaMallocHost(&hSendBuf, sendBufSize));
+  CUDA_RUNTIME(cudaMallocHost(&hRecvBuf, recvBufSize));
+  nvtxRangePop();
+
+  std::vector<MPI_Request> sendReqs(commSize, {});
+  std::vector<MPI_Request> recvReqs(commSize, {});
+
+  CUDA_RUNTIME(
+      cudaMemcpy(hSendBuf, sendbuf, sendBufSize, cudaMemcpyDeviceToHost));
+
+  CUDA_RUNTIME(
+      cudaMemcpy(recvbuf, hRecvBuf, recvBufSize, cudaMemcpyHostToDevice));
+
+  // start remote comms first. if this isn't MPI_COMM_WORLD, it will still be
+  // correct, just random which are first and second
+  for (int j = 0; j < commSize; ++j) {
+    MPI_Isend(((char *)sendbuf) + sdispls[j], sendcounts[j], sendtype, j, 0,
+              comm, &sendReqs[j]);
+  }
+  for (int i = 0; i < commSize; ++i) {
+    {
+      int e = MPI_Irecv(((char *)recvbuf) + rdispls[i], recvcounts[i], recvtype,
+                        i, 0, comm, &recvReqs[i]);
+      err = (MPI_SUCCESS == e ? err : e);
+    }
+  }
+  {
+    int e = MPI_Waitall(sendReqs.size(), sendReqs.data(), MPI_STATUS_IGNORE);
+    err = (MPI_SUCCESS == e ? err : e);
+  }
+  {
+    int e = MPI_Waitall(recvReqs.size(), recvReqs.data(), MPI_STATUS_IGNORE);
+    err = (MPI_SUCCESS == e ? err : e);
+  }
+
+  nvtxRangePush("free");
+  CUDA_RUNTIME(cudaFreeHost(hSendBuf));
+  CUDA_RUNTIME(cudaFreeHost(hRecvBuf));
+  nvtxRangePop();
+
   return err;
 }
 
@@ -184,7 +241,6 @@ int alltoallv_isend_irecv(PARAMS) {
     err = (MPI_SUCCESS == e ? err : e);
   }
 
-
   {
     nvtxRangePush("waitall (send)");
     int e = MPI_Waitall(sendReqs.size(), sendReqs.data(), MPI_STATUS_IGNORE);
@@ -234,7 +290,8 @@ extern "C" int MPI_Alltoallv(PARAMS) {
     return fn(ARGS);
   } else {
     // return alltoallv_remote_first(ARGS);
-    return alltoallv_staged(ARGS);
-    //return alltoallv_isend_irecv(ARGS);
+    // return alltoallv_staged(ARGS);
+    return alltoallv_staged_isir(ARGS);
+    // return alltoallv_isend_irecv(ARGS);
   }
 }
