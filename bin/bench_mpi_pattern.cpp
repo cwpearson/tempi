dacd8a11af751cb09c249d91f8cbe7ccfc4e30fb
Carl Pearson <carl.w.pearson@gmail.com>
2020-10-25 14:29:03 -0600

    dist_graph_reorder pattern

--- bin/bench_mpi_pattern.cpp
+++ bin/bench_mpi_pattern.cpp
@@ -316,6 +316,212 @@ BenchResult bench_sparse_isend_irecv(const SquareMat &mat, const int nIters) {
   return result;
 }
 
+/* use isend/irecv from a single buffer, but don't do anything for zero-size
+ */
+BenchResult bench_dist_graph_reorder(const SquareMat &mat, const int nIters) {
+  BenchResult result{};
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  if (mat.size() != size) {
+    LOG_FATAL("size mismatch");
+    exit(1);
+  }
+
+  // if the matrix is empty, bail
+  bool empty = true;
+  for (size_t i = 0; i < size; ++i) {
+    for (size_t j = 0; j < size; ++j) {
+      if (0 != mat[i][j]) {
+        empty = false;
+      }
+    }
+  }
+  if (empty) {
+    return result;
+  }
+
+#if 0
+  if (0 == rank) {
+    std::cerr << "\nmat\n";
+    for (size_t i = 0; i < size; ++i) {
+      for (size_t j = 0; j < size; ++j) {
+        std::cerr << mat[i][j] << " ";
+      }
+      std::cerr << "\n";
+    }
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+#endif
+
+  // create the new communicator
+  MPI_Comm graph;
+  std::vector<int> sources;
+  std::vector<int> sourceweights;
+  std::vector<int> destinations;
+  std::vector<int> destweights;
+  int graphRank;
+
+  {
+    for (size_t i = 0; i < size; ++i) {
+      if (0 != mat[rank][i]) {
+        destinations.push_back(i);
+        destweights.push_back(mat[rank][i]);
+      }
+      if (0 != mat[i][rank]) {
+        sources.push_back(i);
+        sourceweights.push_back(mat[i][rank]);
+      }
+    }
+
+// print sources
+#if 0
+    for (int i = 0; i < size; ++i) {
+      MPI_Barrier(MPI_COMM_WORLD);
+      if (i == rank) {
+        std::cerr << "rank " << rank << " sources: ";
+        for (auto &e : sources) {
+          std::cerr << e << " ";
+        }
+        std::cerr << "\n";
+      }
+    }
+#endif
+
+// print destinations
+#if 0
+    for (int i = 0; i < size; ++i) {
+      MPI_Barrier(MPI_COMM_WORLD);
+      if (i == rank) {
+        std::cerr << "rank " << rank << " destinations: ";
+        for (auto &e : destinations) {
+          std::cerr << e << " ";
+        }
+        std::cerr << "\n";
+      }
+    }
+#endif
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Dist_graph_create_adjacent(
+        MPI_COMM_WORLD, sources.size(), sources.data(), sourceweights.data(),
+        destinations.size(), destinations.data(), destweights.data(),
+        MPI_INFO_NULL, 1 /*reorder*/, &graph);
+    MPI_Comm_rank(graph, &graphRank);
+  }
+
+  // map MPI_COMM_WORLD rank to graph rank
+  std::vector<int> graphRanks(size); // get graph rank from world rank
+  std::vector<int> worldRanks(size); // get world rank from graph rank
+  MPI_Allgather(&graphRank, 1, MPI_INT, graphRanks.data(), 1, MPI_INT,
+                MPI_COMM_WORLD);
+  MPI_Allgather(&rank, 1, MPI_INT, worldRanks.data(), 1, MPI_INT, graph);
+
+// debug print
+#if 0
+  {
+    if (0 == rank) {
+      std::cerr << "graphRanks: ";
+      for (auto &e : graphRanks) {
+        std::cerr << e << " ";
+      }
+      std::cerr << "\n";
+
+      std::cerr << "worldRanks: ";
+      for (auto &e : worldRanks) {
+        std::cerr << e << " ";
+      }
+      std::cerr << "\n";
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+#endif
+
+  // update neighbor orders (it is not defined to be the same as graph factory
+  MPI_Dist_graph_neighbors(graph, sources.size(), sources.data(),
+                           sourceweights.data(), destinations.size(),
+                           destinations.data(), destweights.data());
+
+  // create GPU allocations
+  size_t sendBufSize = 0;
+  for (size_t i = 0; i < size; ++i) {
+    sendBufSize += mat[rank][i];
+  }
+  size_t recvBufSize = 0;
+  for (size_t i = 0; i < size; ++i) {
+    recvBufSize += mat[i][rank];
+  }
+
+// debug print
+#if 0
+  {
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (int i = 0; i < size; ++i) {
+      MPI_Barrier(MPI_COMM_WORLD);
+      if (i == rank) {
+        std::cerr << "rank " << rank << " sendBufSize,recvBufSize=";
+        std::cerr << sendBufSize << "," << recvBufSize << "\n";
+      }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+#endif
+
+  // create device allocations
+  char *sendbuf = {}, *recvbuf = {};
+  CUDA_RUNTIME(cudaSetDevice(0));
+  CUDA_RUNTIME(cudaMalloc(&sendbuf, sendBufSize));
+  CUDA_RUNTIME(cudaMalloc(&recvbuf, recvBufSize));
+
+  // create Alltoallv arguments
+  std::vector<int> sendcounts, sdispls, recvcounts, rdispls;
+  for (size_t i = 0; i < destinations.size(); ++i) {
+    const int dest = destinations[i];
+    sendcounts.push_back(mat[rank][worldRanks[dest]]);
+  }
+  sdispls.push_back(0);
+  for (size_t i = 1; i < destinations.size(); ++i) {
+    sdispls.push_back(sdispls[i - 1] + sendcounts[i - 1]);
+  }
+  for (size_t i = 0; i < sources.size(); ++i) {
+    const int src = sources[i];
+    recvcounts.push_back(mat[worldRanks[src]][rank]);
+  }
+  rdispls.push_back(0);
+  for (size_t i = 1; i < sources.size(); ++i) {
+    rdispls.push_back(rdispls[i - 1] + recvcounts[i - 1]);
+  }
+
+  // benchmark loop
+  Statistics stats;
+  for (int i = 0; i < nIters; ++i) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    nvtxRangePush("alltoallv");
+    auto start = Clock::now();
+    MPI_Neighbor_alltoallv(sendbuf, sendcounts.data(), sdispls.data(), MPI_BYTE,
+                           recvbuf, recvcounts.data(), rdispls.data(), MPI_BYTE,
+                           graph);
+    auto stop = Clock::now();
+    nvtxRangePop();
+    Duration dur = stop - start;
+    double tmp = dur.count();
+
+    MPI_Allreduce(MPI_IN_PLACE, &tmp, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    stats.insert(tmp);
+  }
+
+  result.minTime = stats.min();
+  result.numBytes = sendBufSize;
+  MPI_Allreduce(MPI_IN_PLACE, &result.numBytes, 1, MPI_UINT64_T, MPI_SUM,
+                MPI_COMM_WORLD);
+
+  CUDA_RUNTIME(cudaFree(sendbuf));
+  CUDA_RUNTIME(cudaFree(recvbuf));
+
+  return result;
+}
+
 int main(int argc, char **argv) {
 
   environment::noTempi = false; // enable at init
@@ -333,9 +539,13 @@ int main(int argc, char **argv) {
   }
 
   std::vector<Benchmark> benchmarks{
+      // clang-format off
       {bench_alltoallv, "alltoallv"},
       {bench_isend_irecv, "isend_irecv"},
-      {bench_sparse_isend_irecv, "sp_isend_irecv"}};
+      {bench_sparse_isend_irecv, "sp_isend_irecv"},
+      {bench_dist_graph_reorder, "dist_graph_reorder"}
+      // clang-format on
+  };
 
   MPI_Barrier(MPI_COMM_WORLD);
 
@@ -344,7 +554,7 @@ int main(int argc, char **argv) {
   std::vector<bool> tempis{true, false};
   std::vector<int64_t> scales{1,         10,         100,        1 * 1000,
                               10 * 1000, 100 * 1000, 1000 * 1000};
-  std::vector<float> densities{0.05, 0.1, 0.5, 1.0};
+  std::vector<float> densities{1.0, 0.5, 0.1, 0.05};
 
   // add some more densities to target particular nnz per row
   for (int targetNnzPerRow : {1, 2, 4, 8, 16}) {
