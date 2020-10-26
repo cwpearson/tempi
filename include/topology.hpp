00e6e91acdd42dda0ae0f9d0a7bb051b14fc201b
Carl Pearson <carl.w.pearson@gmail.com>
2020-10-26 09:15:31 -0600

    use cache_communicator

--- include/topology.hpp
+++ include/topology.hpp
@@ -7,3 +7,6 @@ void topology_init();
 // true if this rank is colocated with other for `comm`
 bool is_colocated(MPI_Comm comm, int other);
 
+namespace topology {
+void cache_communicator(MPI_Comm comm);
+} // namespace topology
--- src/dist_graph_create_adjacent.cpp
+++ src/dist_graph_create_adjacent.cpp
@@ -1,5 +1,6 @@
 #include "env.hpp"
 #include "symbols.hpp"
+#include "topology.hpp"
 
 extern "C" int
 MPI_Dist_graph_create_adjacent(PARAMS_MPI_Dist_graph_create_adjacent) {
@@ -11,6 +12,13 @@ MPI_Dist_graph_create_adjacent(PARAMS_MPI_Dist_graph_create_adjacent) {
   // call the underlying impl to actually create a communicator
   int err = libmpi.MPI_Dist_graph_create_adjacent(
       ARGS_MPI_Dist_graph_create_adjacent);
+
+  // get the topology for the new communicator
+  topology::cache_communicator(*comm_dist_graph);
+
+  // graph partitioning
+  if (reorder) {
+  }
 
   return err;
 }
