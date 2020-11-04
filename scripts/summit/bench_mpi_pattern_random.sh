04fbad51e193d1d6975d2af78031fd9f186a5b59
Carl Pearson <pearson@illinois.edu>
2020-11-03 19:14:22 -0500

    add summit scripts

---
+++ scripts/summit/bench_mpi_pattern_random.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#BSUB -P csc362
+#BSUB -J bench_mpi_pattern_random 
+#BSUB -o bench_mpi_pattern_random.o%J
+#BSUB -e bench_mpi_pattern_random.e%J
+#BSUB -W 01:00
+#BSUB -nnodes 32
+
+set -eou pipefail
+
+module reset
+module unload darshan-runtime
+module load gcc
+module load cuda/11.1.0
+module load nsight-systems/2020.3.1.71
+
+SCRATCH=/gpfs/alpine/scratch/cpearson/csc362/tempi_results
+OUT=$SCRATCH/bench_mpi_pattern_random.csv
+
+set -x
+
+mkdir -p $SCRATCH
+
+export TEMPI_PLACEMENT_METIS=""
+
+#echo "2nodes,6rankpernode" >> $OUT
+#jsrun --smpiargs="-gpu" -n 12 -r 6 -a 1 -g 1 -c 7 -b rs \
+#nsys profile -t cuda,nvtx -f true -o $SCRATCH/tempi_pattern_2n_12r_%q{OMPI_COMM_WORLD_RANK}.qdrep ../../build/bin/bench-mpi-pattern-random | tee -a $OUT
+
+echo "" > $OUT
+
+for nodes in 1 2 4 8 16 32; do
+  for rpn in 1 2 6; do
+    let n=$rpn*$nodes
+
+    echo ${nodes}nodes,${rpn}rankspernode,tempi >> $OUT
+    unset TEMPI_DISABLE
+    jsrun --smpiargs="-gpu" -n $n -r $rpn -a 1 -g 1 -c 7 -b rs ../../build/bin/bench-mpi-pattern-random | tee -a $OUT
+
+    echo ${nodes}nodes,${rpn}rankspernode,notempi >> $OUT
+    export TEMPI_DISABLE=""
+    jsrun --smpiargs="-gpu" -n $n -r $rpn -a 1 -g 1 -c 7 -b rs ../../build/bin/bench-mpi-pattern-random | tee -a $OUT
+  done
+done
+
