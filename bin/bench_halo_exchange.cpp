a3c4c8043fe2f1de5868489882b0b73a3e1d983e
Carl Pearson <carl.w.pearson@gmail.com>
2020-11-09 15:22:50 -0700

    progress

--- bin/bench_halo_exchange.cpp
+++ bin/bench_halo_exchange.cpp
@@ -1,3 +1,5 @@
+#include "statistics.hpp"
+
 #include <cuda_runtime.h>
 #include <mpi.h>
 
@@ -5,7 +7,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <map>
 #include <vector>
+
+// GPU or not
+#define USE_CUDA
 
 inline void checkCuda(cudaError_t result, const char *file, const int line) {
   if (result != cudaSuccess) {
@@ -14,7 +20,6 @@ inline void checkCuda(cudaError_t result, const char *file, const int line) {
     exit(-1);
   }
 }
-
 #define CUDA_RUNTIME(stmt) checkCuda(stmt, __FILE__, __LINE__);
 
 cudaExtent div_cudaExtent(cudaExtent &a, cudaExtent &b) {
@@ -43,13 +48,18 @@ void to_1d(int *d1, const int d3[3], const int dim[3]) {
   assert(*d1 < dim[0] * dim[1] * dim[2]);
 }
 
-/* return the halo size in bytes for a region of size `ext`, with `radius`, for
+/* return the halo size in bytes for a region of size `lcr`, with `radius`, for
  * a send in direction `dir`
  */
-size_t halo_size(const int radius, cudaExtent ext, const int dir[3]) {
-  size_t ret = 0;
+size_t halo_size(const int radius,
+                 const int lcr[3], // size of the local compute region
+                 const int dir[3]) {
 
-  return ret;
+  int ext[3]{};
+  ext[0] = (0 == dir[0]) ? lcr[0] : radius;
+  ext[1] = (0 == dir[1]) ? lcr[1] : radius;
+  ext[2] = (0 == dir[2]) ? lcr[2] : radius;
+  return ext[0] * ext[1] * ext[2];
 }
 
 /* create a derived datatype describing the particular direction
@@ -129,9 +139,14 @@ MPI_Datatype halo_type(const int radius, cudaPitchedPtr curr,
   return cubetype;
 }
 
-struct BenchResult {};
+struct BenchResult {
+  Statistics pack;
+  Statistics alltoallv;
+  Statistics unpack;
+  Statistics comm;
+};
 
-BenchResult bench(MPI_Comm comm, int nquants, int radius) {
+BenchResult bench(MPI_Comm comm, int nquants, int radius, int nIters) {
 
   BenchResult result;
 
@@ -173,7 +188,17 @@ BenchResult bench(MPI_Comm comm, int nquants, int radius) {
     e.width += 2 * radius;
     e.height += 2 * radius;
     e.depth += 2 * radius;
+
+#ifdef USE_CUDA
     CUDA_RUNTIME(cudaMalloc3D(&curr, e));
+#else
+    // smallest multiple of 512 >= pitch (to match CUDA)
+    size_t pitch = (e.width + 511) / 512 * 512;
+    curr.pitch = pitch;
+    curr.ptr = new char[pitch * e.height * e.depth];
+    curr.xsize = e.width;
+    curr.ysize = e.height;
+#endif
   }
   if (0 == rank) {
 
@@ -182,13 +207,16 @@ BenchResult bench(MPI_Comm comm, int nquants, int radius) {
   }
   MPI_Barrier(MPI_COMM_WORLD);
 
-  // my coordinates in the distributed space
+  /* have each rank take the role of a particular compute region to build the
+   * communicator.
+   *
+   * convert this rank directly into a 3d coordinate
+   */
   int mycoord[3];
   to_3d(mycoord, rank, dims);
 
-  std::vector<int> nbrs; // ranks of my neighbors
-  std::vector<MPI_Datatype> nbrSendType,
-      nbrRecvType; // the type I send/recv for each direction
+  // figure out how much I communicate with each neighbor
+  std::map<int, int> nbrSendWeight, nbrRecvWeight;
   for (int dz = -1; dz <= 1; ++dz) {
     for (int dy = -1; dy <= 1; ++dy) {
       for (int dx = -1; dx <= 1; ++dx) {
@@ -196,10 +224,88 @@ BenchResult bench(MPI_Comm comm, int nquants, int radius) {
           continue;
         }
 
+        int dir[3]{};
+        dir[0] = dx;
+        dir[1] = dy;
+        dir[2] = dz;
+
+        // determine the coordinate and rank of each neighbor
+        int nbrcoord[3]{}, nbrRank = -1;
+        nbrcoord[0] = mycoord[0] + dir[0];
+        nbrcoord[1] = mycoord[1] + dir[1];
+        nbrcoord[2] = mycoord[2] + dir[2];
+        while (nbrcoord[0] < 0) {
+          nbrcoord[0] += dims[0];
+        }
+        while (nbrcoord[1] < 0) {
+          nbrcoord[1] += dims[1];
+        }
+        while (nbrcoord[2] < 0) {
+          nbrcoord[2] += dims[2];
+        }
+        nbrcoord[0] %= dims[0];
+        nbrcoord[1] %= dims[1];
+        nbrcoord[2] %= dims[2];
+        to_1d(&nbrRank, nbrcoord, dims);
+
+        int weight = halo_size(radius, lcr, dir);
+        nbrSendWeight[nbrRank] += weight;
+        nbrRecvWeight[nbrRank] += weight;
+      }
+    }
+  }
+
+  // create topology
+  MPI_Comm graphComm;
+  {
+    MPI_Comm comm_old = MPI_COMM_WORLD;
+    int indegree = nbrRecvWeight.size();
+    std::vector<int> sources;
+    std::vector<int> sourceweights;
+    for (auto &kv : nbrRecvWeight) {
+      sources.push_back(kv.first);
+      sourceweights.push_back(kv.second);
+    }
+
+    int outdegree = nbrSendWeight.size();
+    std::vector<int> destinations;
+    std::vector<int> destweights;
+    for (auto &kv : nbrSendWeight) {
+      destinations.push_back(kv.first);
+      destweights.push_back(kv.second);
+    }
+
+    MPI_Info info = MPI_INFO_NULL;
+    int reorder = 1;
+    double start = MPI_Wtime();
+    MPI_Dist_graph_create_adjacent(
+        comm_old, indegree, sources.data(), sourceweights.data(), outdegree,
+        destinations.data(), destweights.data(), info, reorder, &graphComm);
+    result.comm.insert(MPI_Wtime() - start);
+  }
+
+  // get my new rank after reorder
+  MPI_Comm_rank(graphComm, &rank);
+  to_3d(mycoord, rank, dims);
+
+  // construct datatypes for each halo direction
+  std::map<int, std::vector<MPI_Datatype>> nbrSendType, nbrRecvType;
+  for (int dz = -1; dz <= 1; ++dz) {
+    for (int dy = -1; dy <= 1; ++dy) {
+      for (int dx = -1; dx <= 1; ++dx) {
+        if (!dx && !dy && !dz) {
+          continue;
+        }
+
+        int dir[3];
+        dir[0] = dx;
+        dir[1] = dy;
+        dir[2] = dz;
+
         int nbrcoord[3];
-        nbrcoord[0] = mycoord[0] + dx;
-        nbrcoord[1] = mycoord[1] + dy;
-        nbrcoord[2] = mycoord[2] + dz;
+        nbrcoord[0] = mycoord[0] + dir[0];
+        nbrcoord[1] = mycoord[1] + dir[1];
+        nbrcoord[2] = mycoord[2] + dir[2];
 
         // wrap neighbors
         while (nbrcoord[0] < 0) {
@@ -217,153 +323,255 @@ BenchResult bench(MPI_Comm comm, int nquants, int radius) {
 
         int nbrRank;
         to_1d(&nbrRank, nbrcoord, dims);
-        nbrs.push_back(nbrRank);
-        int dir[3];
-        dir[0] = dx;
-        dir[1] = dy;
-        dir[2] = dz;
+
         MPI_Datatype interior = halo_type(radius, curr, lcr, dir, false);
         MPI_Type_commit(&interior);
         MPI_Datatype exterior = halo_type(radius, curr, lcr, dir, true);
         MPI_Type_commit(&exterior);
 
+        // for periodic, send should always be the same as recv
         {
           int sendSize, recvSize;
           MPI_Type_size(interior, &sendSize);
           MPI_Type_size(exterior, &recvSize);
           assert(sendSize == recvSize);
         }
+
+        nbrSendType[nbrRank].push_back(interior);
+        nbrRecvType[nbrRank].push_back(exterior);
       }
     }
   }
 
   // print neighbors
   for (int r = 0; r < size; ++r) {
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(graphComm);
     if (rank == r) {
       std::cout << "rank " << rank << " nbrs= ";
-      for (int n : nbrs) {
-        std::cout << n << " ";
+      for (const auto &kv : nbrSendType) {
+        std::cout << kv.first << " ";
       }
       std::cout << "\n";
     }
   }
   std::cout << std::flush;
+
+  std::vector<int> sources(nbrRecvType.size(), -1);
+  std::vector<int> sourceweights(nbrRecvType.size(), -1);
+  std::vector<int> destinations(nbrSendType.size(), -1);
+  std::vector<int> destweights(nbrSendType.size(), -1);
+  MPI_Dist_graph_neighbors(graphComm, sources.size(), sources.data(),
+                           sourceweights.data(), destinations.size(),
+                           destinations.data(), destweights.data());
 
   // print volumes
   for (int r = 0; r < size; ++r) {
+    MPI_Barrier(graphComm);
+    if (rank == r) {
+      std::cout << "rank " << rank << " sizes= ";
+      for (const auto &kv : nbrSendType) {
+        for (MPI_Datatype ty : kv.second) {
+          int size;
+          MPI_Type_size(ty, &size);
+          std::cout << size << " ";
+        }
+      }
+      std::cout << "\n";
+    }
+  }
+  std::cout << std::flush;
+
+  // print extents
+#if 0
+  for (int r = 0; r < size; ++r) {
     MPI_Barrier(MPI_COMM_WORLD);
     if (rank == r) {
-      std::cout << "rank " << rank << " bufSize= ";
-      for (MPI_Datatype ty : nbrSendType) {
+      std::cout << "rank " << rank << " extents= ";
+      for (const auto &kv : nbrSendType) {
+        for (MPI_Datatype ty : kv.second) {
+          MPI_Aint lb, extent;
+          MPI_Type_get_extent(ty, &lb, &extent);
+          std::cout << "[" << lb << "," << lb + extent << ") ";
+        }
+      }
+      std::cout << "\n";
+    }
+  }
+  std::cout << std::flush;
+#endif
+
+  // create the total send/recv buffer
+  std::vector<char> sendbuf, recvbuf;
+  for (const auto &kv : nbrSendType) {
+    for (MPI_Datatype ty : kv.second) {
+      int size;
+      MPI_Type_size(ty, &size);
+      sendbuf.resize(sendbuf.size() + size);
+    }
+  }
+  for (const auto &kv : nbrRecvType) {
+    for (MPI_Datatype ty : kv.second) {
+      int size;
+      MPI_Type_size(ty, &size);
+      recvbuf.resize(recvbuf.size() + size);
+    }
+  }
+
+// print buffer sizes
+#if 1
+  std::cout << "rank " << rank << " sendbuf=" << sendbuf.size() << "\n";
+  std::cout << "rank " << rank << " recvbuf=" << recvbuf.size() << "\n";
+  std::cout << std::flush;
+#endif
+
+  // pack data for each neighbor and determine offsets in send buffer
+  std::vector<int> sendcounts;
+  {
+    for (int nbr : destinations) {
+      int sendcount = 0;
+      for (MPI_Datatype ty : nbrSendType[nbr]) {
         int size;
         MPI_Type_size(ty, &size);
-        std::cout << size << " ";
+        sendcount += size;
       }
-      std::cout << "\n";
+      sendcounts.push_back(sendcount);
     }
   }
-  std::cout << std::flush;
+  std::vector<int> sdispls(sendcounts.size(), 0);
+  for (size_t i = 1; i < sdispls.size(); ++i) {
+    sdispls[i] = sdispls[i - 1] + sendcounts[i - 1];
+  }
 
-// create topology
-#if 0
-{
-MPI_Comm comm_old = MPI_COMM_WORLD;
- intindegree, constintsources[], constintsourceweights[],
-      intoutdegree, constintdestinations[], constintdestweights[], MPI_Infoinfo,
-      intreorder, MPI_Comm * comm_dist_graph
-
-  MPI_Dist_graph_create_adjacent(
-      comm_old, indegree, sources, sourceweights,
-      outdegree, destinations, destweights, info,
-      reorder, comm_dist_graph)
-}
+#if 1
+  std::cerr << "rank " << rank << " sendcounts=";
+  for (int e : sendcounts) {
+    std::cerr << e << " ";
+  }
+  std::cerr << "\n";
+  std::cerr << "rank " << rank << " sdispls=";
+  for (int e : sdispls) {
+    std::cerr << e << " ";
+  }
+  std::cerr << "\n";
 #endif
 
-#if 0
-
-
-
-  // make my own allocation
-  std::vector<cudaPitchedPtr> data(nquants);
-  for (auto &ptr : data) {
-    CUDA_RUNTIME(cudaMalloc3D(&ptr, rankSize));
+  // determine recv counts and displacements
+  std::vector<int> recvcounts;
+  for (int nbr : sources) {
+    int recvcount = 0;
+    for (MPI_Datatype ty : nbrRecvType[nbr]) {
+      int size;
+      MPI_Type_size(ty, &size);
+      recvcount += size;
+    }
+    recvcounts.push_back(recvcount);
+  }
+  std::vector<int> rdispls(recvcounts.size(), 0);
+  for (size_t i = 1; i < rdispls.size(); ++i) {
+    rdispls[i] = rdispls[i - 1] + recvcounts[i - 1];
   }
 
-  for (int r = 0; r < size; ++r) {
-    MPI_Barrier(cart);
-    if (rank == r) {
-      int neighbors int dst;
-      MPI_Cart_shift(cart, 0 /*dir*/, 1 /*displ*/, &src, &dst);
-      std::cout << "rank " << rank << " +0: ";
-      if (src >= 0)
-        std::cout << src;
-      else
-        std::cout << "x";
-      std::cout << " -> " << rank << " -> ";
-      if (dst >= 0)
-        std::cout << dst;
-      else
-        std::cout << "x";
-      std::cout << "\n";
-    }
+#if 1
+  std::cerr << "rank " << rank << " recvcounts=";
+  for (int e : recvcounts) {
+    std::cerr << e << " ";
   }
-  std::cout << std::flush;
-
-  /* what is the order of ranks
-  source and dest for disp=1 for each dimension?
-
-  we'll use a single type to describe each halo direction, so the send count
-  will always be 0 (if no nbr) or 1
-  */
-  int sendcounts[3 * 2]{0};
-  int recvcounts[3 * 2]{0};
-  for (int dir = 0; dir < 3; ++dir) {
-    int src, dst;
-    MPI_Cart_shift(cart, dir, 1 /*displ*/, &src, &dst);
-    // set send count to 1 if a neighbor exists
-    if (src >= 0) {
-      recvcounts[dir * 2] = 1;
-      sendcounts[dir * 2] = 1;
-    }
-    if (dst >= 0) {
-      recvcounts[dir * 2 + 1] = 1;
-      sendcounts[dir * 2 + 1] = 1;
-    }
+  std::cerr << "\n";
+  std::cerr << "rank " << rank << " rdispls=";
+  for (int e : rdispls) {
+    std::cerr << e << " ";
   }
+  std::cerr << "\n";
+#endif
 
-  for (int r = 0; r < size; ++r) {
-    MPI_Barrier(cart);
-    if (rank == r) {
-      std::cout << "rank " << rank << " sendcounts=";
-      for (int i = 0; i < 3 * 2; ++i) {
-        std::cout << sendcounts[i] << " ";
+  for (int i = 0; i < nIters; ++i) {
+
+    std::cerr << "iter " << i << std::endl;
+
+    // pack the send buf
+    {
+      double start = MPI_Wtime();
+      int position = 0;
+      for (int nbr : destinations) {
+        for (MPI_Datatype ty : nbrSendType[nbr]) {
+          MPI_Pack(curr.ptr, 1, ty, sendbuf.data(), sendbuf.size(), &position,
+                   graphComm);
+        }
       }
-      std::cout << std::endl;
+      result.pack.insert(MPI_Wtime() - start);
+    }
+
+    // exchange
+    {
+
+      double start = MPI_Wtime();
+      MPI_Neighbor_alltoallv(sendbuf.data(), sendcounts.data(), sdispls.data(),
+                             MPI_BYTE, recvbuf.data(), recvcounts.data(),
+                             rdispls.data(), MPI_BYTE, graphComm);
+      result.alltoallv.insert(MPI_Wtime() - start);
+    }
+
+    // unpack recv buf
+    {
+      double start = MPI_Wtime();
+      int position = 0;
+      for (int nbr : sources) {
+        for (MPI_Datatype ty : nbrRecvType[nbr]) {
+          MPI_Unpack(recvbuf.data(), recvbuf.size(), &position, curr.ptr, 1, ty,
+                     graphComm);
+        }
+      }
+      result.unpack.insert(MPI_Wtime() - start);
     }
   }
-  std::cout << std::flush;
 
-#if 0
-  MPI_Neighbor_alltoallw(
-      const void *sendbuf, const int sendcounts[], const MPI_Aint sdispls[],
-      const MPI_Datatype sendtypes[], void *recvbuf, const int recvcounts[],
-      const MPI_Aint rdispls[], const MPI_Datatype recvtypes[], MPI_Comm comm)
+  MPI_Comm_free(&graphComm);
+  graphComm = {};
+
+#ifdef USE_CUDA
+  CUDA_RUNTIME(cudaFree(curr.ptr));
+#else
+  delete[] curr.ptr;
 #endif
 
-  MPI_Comm_free(&cart);
-
-  // free device allocations
-  for (auto &ptr : data) {
-    CUDA_RUNTIME(cudaFree(ptr.ptr));
-  }
-
-#endif
   return result;
 }
 
 int main(void) {
   MPI_Init(nullptr, nullptr);
-  bench(MPI_COMM_WORLD, 2, 2);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  int nIters = 30;
+  int nQuants = 2;
+  int radius = 2;
+
+  if (0 == rank) {
+    std::cout << "comm(s),pack (s),alltoallv (s),unpack (s)\n";
+    std::cout << std::flush;
+  }
+
+  BenchResult result = bench(MPI_COMM_WORLD, nQuants, radius, nIters);
+
+  double pack, alltoallv, unpack, comm;
+
+  {
+    double t1 = result.pack.min();
+    double t2 = result.alltoallv.min();
+    double t3 = result.unpack.min();
+    double t4 = result.comm.min();
+    MPI_Reduce(&t1, &pack, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&t2, &alltoallv, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&t3, &unpack, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&t4, &comm, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+  }
+
+  if (0 == rank) {
+    std::cout << comm << "," << pack << "," << alltoallv << "," << unpack
+              << "\n";
+  }
+
   MPI_Finalize();
 }
