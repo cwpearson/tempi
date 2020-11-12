3de28f14119ac75aecefc7239d9738b99df34ca6
Carl Pearson <carl.w.pearson@gmail.com>
2020-11-12 16:56:15 -0700

    fix expected result

--- test/partition_kahip_process_mapping.cpp
+++ test/partition_kahip_process_mapping.cpp
@@ -1,5 +1,6 @@
-/* don't build this right now since process_mapping often returns unbalanced partitions
-*/
+/* don't build this right now since process_mapping often returns unbalanced
+ * partitions
+ */
 
 #include "../include/partition.hpp"
 #include "../support/csr.hpp"
@@ -12,11 +13,15 @@ CSR make_csr_random_sparse(int numNodes, int ranksPerNode, int rowNnz,
   SquareMat mat = SquareMat::make_random_sparse(numNodes * ranksPerNode, rowNnz,
                                                 1, 10, 1, seed);
 
+  std::cerr << mat.str() << "\n";
+
   // no self edges
   mat.zero_diagonal();
   // forward and back-edges must exist
   // f/b edges must have same weight
   mat.balance_symmetric();
+
+  std::cerr << mat.str() << "\n";
 
   CSR csr(mat);
   REQUIRE(csr.rowPtr.size() == mat.size() + 1);
@@ -34,24 +39,41 @@ int main(void) {
   int matSeed = 101;
 
   for (int numNodes : {1, 2, 4, 8, 16, 32}) {
-    for (int ranksPerNode : {1, 6}) {
+    for (int ranksPerNode : {1, 2, 6}) {
       std::cerr << "TEST partition_kahip " << numNodes << " " << ranksPerNode
                 << " " << matSeed << "\n";
       CSR csr = make_csr_random_sparse(numNodes, ranksPerNode, rowNnz, matSeed);
+
+      std::cerr << "rowPtr: ";
+      for (int e : csr.rowPtr) {
+        std::cerr << e << " ";
+      }
+      std::cerr << "\n";
+
+      std::cerr << "colInd: ";
+      for (int e : csr.colInd) {
+        std::cerr << e << " ";
+      }
+      std::cerr << "\n";
+      std::cerr << "colVal: ";
+      for (int e : csr.colVal) {
+        std::cerr << e << " ";
+      }
+      std::cerr << "\n";
+
       partition::Result result = partition::kahip_process_mapping(
           numNodes, csr.rowPtr, csr.colInd, csr.colVal);
 
-        // std::cerr << "part: ";
-        // for (int e : result.part) {
-        //   std::cerr << e << " ";
-        // }
-        // std::cerr << "\n";
-        // std::cerr << result.num_parts() << "\n";
-        // std::cerr << result.objective << "\n";
+      std::cerr << "part: ";
+      for (int e : result.part) {
+        std::cerr << e << " ";
+      }
+      std::cerr << "\n";
+      std::cerr << result.num_parts() << "\n";
+      std::cerr << result.objective << "\n";
 
-      REQUIRE(result.num_parts() == numNodes);
+      REQUIRE(result.num_parts() == numNodes * ranksPerNode);
       REQUIRE(partition::is_balanced(result));
-
     }
   }
 
