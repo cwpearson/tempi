95030cf6065be1e6403774953d871a331b07bb72
Carl Pearson <pearson@illinois.edu>
2020-11-27 18:19:14 -0500

    overwrite output

--- scripts/summit/bench_mpi_pack.sh
+++ scripts/summit/bench_mpi_pack.sh
@@ -3,7 +3,7 @@
 #BSUB -J bench_mpi_pack 
 #BSUB -o bench_mpi_pack.o%J
 #BSUB -e bench_mpi_pack.e%J
-#BSUB -W 02:00
+#BSUB -W 01:00
 #BSUB -nnodes 1
 #BSUB -alloc_flags gpudefault
 
@@ -11,8 +11,8 @@ set -eou pipefail
 
 module reset
 module unload darshan-runtime
-module load gcc
-module load cuda/11.0.3
+module load gcc/7.4.0
+module load cuda/11.1.1
 
 SCRATCH=/gpfs/alpine/scratch/cpearson/csc362/tempi_results
 OUT=$SCRATCH/bench_mpi_pack.csv
@@ -21,9 +21,14 @@ set -x
 
 mkdir -p $SCRATCH
 
-# CSV header
-echo "x,y,z,nodes,ranks-per-node,self-per-node,colo-per-node,remote-per-node,self,colo,remote,first (s),total (s)" > $OUT
+echo "" > $OUT
 
-jsrun --smpiargs="-gpu" -n 1 -a 1 -g 1 -c 42 -r 1 -b rs ../../build/bin/bench-mpi-pack | tee -a $OUT
+echo "tempi" >> $OUT
+unset TEMPI_DISABLE
+jsrun --smpiargs="-gpu" -n 1 -a 1 -g 1 -c 7 -r 1 -b rs ../../build/bin/bench-mpi-pack | tee -a $OUT
 
+echo "notempi" >> $OUT
+export TEMPI_DISABLE=""
+jsrun --smpiargs="-gpu" -n 1 -a 1 -g 1 -c 7 -r 1 -b rs ../../build/bin/bench-mpi-pack | tee -a $OUT
+unset TEMPI_DISABLE
 
