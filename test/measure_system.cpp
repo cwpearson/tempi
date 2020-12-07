2362b5a87490100c44d4a3b92a98ab6d93ba2fd6
Carl Pearson <pearson@illinois.edu>
2020-12-07 17:06:11 -0500

    .

--- test/measure_system.cpp
+++ test/measure_system.cpp
@@ -3,12 +3,8 @@
 #include "../include/measure_system.hpp"
 
 int main(int argc, char **argv) {
-
   MPI_Init(&argc, &argv);
-
   measure_system(MPI_COMM_WORLD);
-
   MPI_Finalize();
-
   return 0;
 }
