46c0d01f6114e0822a8436b8d64d937f4d3fd30c
K-Wu <11816012+K-Wu@users.noreply.github.com>
2020-12-25 15:30:04 -0600

    add CMakeLists t o let bench_mpi_ireduce compile; format bench_mpi_ireduce

--- bin/bench_mpi_ireduce.cpp
+++ bin/bench_mpi_ireduce.cpp
@@ -20,8 +20,7 @@ struct BenchResult {
 
 BenchResult bench(size_t numBytes,
                   bool tempi, // use tempi or not
-                  const int nIters,
-                  const int mpi_reduction_api_idx,
+                  const int nIters, const int mpi_reduction_api_idx,
                   const int mpi_op_idx) {
 
   // number of overlapping messages
@@ -43,60 +42,59 @@ BenchResult bench(size_t numBytes,
   }
   std::vector<MPI_Request> reqs(tags);
 
-  
   MPI_Op mpi_op_selected;
-  switch(mpi_op_idx){
-    case 0:{
-      mpi_op_selected=MPI_MAX;
-      break;
-    }
-    case 1:{
-      mpi_op_selected=MPI_MIN;
-      break;
-    }
-    case 2:{
-      mpi_op_selected=MPI_SUM;
-      break;
-    }
-    case 3:{
-      mpi_op_selected=MPI_PROD;
-      break;
-    }
-    case 4:{
-      mpi_op_selected=MPI_LAND;
-      break;
-    }
-    case 5:{
-      mpi_op_selected=MPI_BAND;
-      break;
-    }
-    case 6:{
-      mpi_op_selected=MPI_LOR;
-      break;
-    }
-    case 7:{
-      mpi_op_selected=MPI_BOR;
-      break;
-    }
-    case 8:{
-      mpi_op_selected=MPI_LXOR;
-      break;
-    }
-    case 9:{
-      mpi_op_selected=MPI_BXOR;
-      break;
-    }
-    case 10:{
-      mpi_op_selected=MPI_MAXLOC;
-      break;
-    }
-    case 11:{
-      mpi_op_selected=MPI_MINLOC;
-      break;
-    }
-    default:{
-      LOG_FATAL("unrecognized MPI_Op");
-    }
+  switch (mpi_op_idx) {
+  case 0: {
+    mpi_op_selected = MPI_MAX;
+    break;
+  }
+  case 1: {
+    mpi_op_selected = MPI_MIN;
+    break;
+  }
+  case 2: {
+    mpi_op_selected = MPI_SUM;
+    break;
+  }
+  case 3: {
+    mpi_op_selected = MPI_PROD;
+    break;
+  }
+  case 4: {
+    mpi_op_selected = MPI_LAND;
+    break;
+  }
+  case 5: {
+    mpi_op_selected = MPI_BAND;
+    break;
+  }
+  case 6: {
+    mpi_op_selected = MPI_LOR;
+    break;
+  }
+  case 7: {
+    mpi_op_selected = MPI_BOR;
+    break;
+  }
+  case 8: {
+    mpi_op_selected = MPI_LXOR;
+    break;
+  }
+  case 9: {
+    mpi_op_selected = MPI_BXOR;
+    break;
+  }
+  case 10: {
+    mpi_op_selected = MPI_MAXLOC;
+    break;
+  }
+  case 11: {
+    mpi_op_selected = MPI_MINLOC;
+    break;
+  }
+  default: {
+    LOG_FATAL("unrecognized MPI_Op");
+  }
   }
   Statistics stats;
   nvtxRangePush("loop");
@@ -104,16 +102,17 @@ BenchResult bench(size_t numBytes,
     int position = 0;
     auto start = Clock::now();
     for (int i = 0; i < tags; ++i) {
-        if (mpi_reduction_api_idx ==0){
-          MPI_Ireduce(srcs[i], dsts[i], numBytes, MPI_BYTE, mpi_op_selected, 0, MPI_COMM_WORLD, &reqs[i]);
-        }
-        else if (mpi_reduction_api_idx==1){
-          MPI_Iallreduce(srcs[i], dsts[i], numBytes, MPI_BYTE, mpi_op_selected, MPI_COMM_WORLD, &reqs[i]);
-        }
-        else if (mpi_reduction_api_idx==2){
-          MPI_Iscan(srcs[i], dsts[i], numBytes, MPI_BYTE, mpi_op_selected, MPI_COMM_WORLD, &reqs[i]);
-        }
+      if (mpi_reduction_api_idx == 0) {
+        MPI_Ireduce(srcs[i], dsts[i], numBytes, MPI_BYTE, mpi_op_selected, 0,
+                    MPI_COMM_WORLD, &reqs[i]);
+      } else if (mpi_reduction_api_idx == 1) {
+        MPI_Iallreduce(srcs[i], dsts[i], numBytes, MPI_BYTE, mpi_op_selected,
+                       MPI_COMM_WORLD, &reqs[i]);
+      } else if (mpi_reduction_api_idx == 2) {
+        MPI_Iscan(srcs[i], dsts[i], numBytes, MPI_BYTE, mpi_op_selected,
+                  MPI_COMM_WORLD, &reqs[i]);
       }
+    }
     MPI_Waitall(tags, reqs.data(), MPI_STATUS_IGNORE);
     auto stop = Clock::now();
     Duration dur = stop - start;
@@ -161,20 +160,23 @@ int main(int argc, char **argv) {
 
     std::vector<bool> tempis = {true, false};
 
-    constexpr std::array<int,3> mpi_reduction_apis={0,1,2};
-    constexpr std::array<int,12> mpi_reduction_ops={0,1,2,3,4,5,6,7,8,9,10,11};
+    constexpr std::array<int, 3> mpi_reduction_apis = {0, 1, 2};
+    constexpr std::array<int, 12> mpi_reduction_ops = {0, 1, 2, 3, 4,  5,
+                                                       6, 7, 8, 9, 10, 11};
 
     if (0 == rank) {
       std::cout << "n,tempi, MiB/s\n";
     }
 
-    for (int mpi_reduction_api_idx: mpi_reduction_apis){
-      for (int mpi_reduction_op_idx: mpi_reduction_ops){
+    for (int mpi_reduction_api_idx : mpi_reduction_apis) {
+      for (int mpi_reduction_op_idx : mpi_reduction_ops) {
         for (bool tempi : tempis) {
           for (int n : ns) {
 
             std::string s;
-            s = std::to_string(mpi_reduction_api_idx) + std::to_string(mpi_reduction_op_idx) + std::to_string(n) + "/" + std::to_string(tempi);
+            s = std::to_string(mpi_reduction_api_idx) +
+                std::to_string(mpi_reduction_op_idx) + std::to_string(n) + "/" +
+                std::to_string(tempi);
 
             if (0 == rank) {
               std::cout << s;
@@ -186,10 +188,12 @@ int main(int argc, char **argv) {
             }
 
             nvtxRangePush(s.c_str());
-            result = bench(n, tempi, nIters,mpi_reduction_api_idx,mpi_reduction_op_idx);
+            result = bench(n, tempi, nIters, mpi_reduction_api_idx,
+                           mpi_reduction_op_idx);
             nvtxRangePop();
             if (0 == rank) {
-              std::cout << "," << 2 * double(n) / 1024 / 1024 / result.pingPongTime;
+              std::cout << ","
+                        << 2 * double(n) / 1024 / 1024 / result.pingPongTime;
               std::cout << std::flush;
             }
 
