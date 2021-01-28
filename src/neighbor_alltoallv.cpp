183ec3cfd727ae17088ab22bcdf0958110c2374f
Carl Pearson <carl.w.pearson@gmail.com>
2021-01-28 13:22:38 -0700

    remove unused code

--- src/neighbor_alltoallv.cpp
+++ src/neighbor_alltoallv.cpp
@@ -14,35 +14,11 @@ extern "C" int MPI_Neighbor_alltoallv(PARAMS_MPI_Neighbor_alltoallv) {
     return libmpi.MPI_Neighbor_alltoallv(ARGS_MPI_Alltoallv);
   }
 
-#if TEMPI_OUTPUT_LEVEL >= 4
-  {
-    auto it = degrees.find(comm);
-    if (it != degrees.end()) {
-
       /* this call does not take ranks, so there is no need to handle
          reordering. The library ranks are different than the application ranks,
          but they have the right neighbors in a consistent order, just with
          different numbers
       */
-
-      {
-        std::string s;
-        for (int i = 0; i < it->second.indegree; ++i) {
-          s += std::to_string(sendcounts[i]) + " ";
-        }
-        LOG_SPEW("sendcounts=" << s);
-      }
-
-      {
-        std::string s;
-        for (int i = 0; i < it->second.outdegree; ++i) {
-          s += std::to_string(recvcounts[i]) + " ";
-        }
-        LOG_SPEW("recvcounts=" << s);
-      }
-    }
-  }
-#endif
 
   return libmpi.MPI_Neighbor_alltoallv(ARGS_MPI_Neighbor_alltoallv);
 }
