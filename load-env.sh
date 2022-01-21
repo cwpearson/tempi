350587d7ff598bde52d154896f704bb006b1d71d
Carl William Pearson <cwpears@sandia.gov>
2022-01-21 14:07:45 -0700

    ../load-env.sh
    Sandia vortex configuration

---
+++ load-env.sh
@@ -0,0 +1,30 @@
+host=`hostname`
+
+if [[ "$host" =~ .*vortex.* ]]; then
+# CUDA 10.1 & cmake 3.18.0 together cause some problem with recognizing the `-pthread` flag.
+
+    echo "$host" matched vortex
+    
+    echo "export CUDAARCHS=70"
+    export CUDAARCHS="70" # for cmake 3.20+
+
+    echo module --force purge
+    module --force purge
+
+    echo module load cmake/3.18.0
+    module load cmake/3.18.0
+    echo module load cuda/10.2.89
+    module load cuda/10.2.89
+    echo module load gcc/8.3.1
+    module load gcc/8.3.1
+    echo module load spectrum-mpi/2020.08.19
+    module load spectrum-mpi/2020.08.19
+
+    #export MPI_ROOT=$HOME/software/openmpi3.1.4-cuda10.2.89-gcc7.3.1
+    #export LD_LIBRARY_PATH=$HOME/software/openmpi3.1.4-cuda10.2.89-gcc7.3.1/lib
+
+    which cmake
+    which gcc
+    which nvcc
+    which mpirun
+fi
